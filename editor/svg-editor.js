ca0589c4045f90058e7e8deefa1c1d08588dbd38
Mark MacKay <mark@duopixel.com>
2012-06-27 13:23:59 -0500

    a better way of detecting iframe focus

--- editor/svg-editor.js
+++ editor/svg-editor.js
@@ -2055,8 +2055,12 @@
         return false;
       }
       var closer = function(e){
+        
+        //Method of Action specific to detect when the iframe has focus
         if (top != self && !e.isTrigger) {
-            top.exports.setEditorFocus();
+            if(typeof top.exports.setEditorFocus == 'function') {
+              top.exports.setEditorFocus();
+            }
         }
           if (!$(e.target).hasClass("menu_title") && $('#menu_bar').hasClass("active")) {
             if(!$(e.target).hasClass("disabled") && $(e.target).hasClass("menu_item")) {
