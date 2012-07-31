962d755d896ce8c5c5cc6aaaf726b335dbc9f623
Mark MacKay <mark@duopixel.com>
2012-07-31 04:36:50 -0500

    firefox compiled fix

--- editor/browser.js
+++ editor/browser.js
@@ -84,7 +84,8 @@ var supportsGoodTextCharPos_ = (function() {
 	var text = document.createElementNS(svgns,'text');
 	text.textContent = 'a';
 	svgcontent.appendChild(text);
-	var pos = text.getStartPositionOfChar(0).x;
+	var pos = text.getStartPositionOfChar(0)
+	pos = pos.x; //if you put it on one line it fails when compiled
 	document.documentElement.removeChild(svgroot);
 	return (pos === 0);
 })();
