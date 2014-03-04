0a0af3473780640de3352eeba86b843180b143e4
techninja <james@tn42.com>
2014-03-03 20:55:33 -0800

    Pull default attr values from element itself during convert to path

--- editor/src/svgcanvas.js
+++ editor/src/svgcanvas.js
@@ -7888,16 +7888,18 @@ this.convertToPath = function(elem, getBBox) {
     var batchCmd = new BatchCommand("Convert element to Path");
   }
 
+  var $elem = $(elem);
+
   var attrs = getBBox?{}:{
-    "fill": cur_shape.fill,
-    "fill-opacity": cur_shape.fill_opacity,
-    "stroke": cur_shape.stroke,
-    "stroke-width": cur_shape.stroke_width,
-    "stroke-dasharray": cur_shape.stroke_dasharray,
-    "stroke-linejoin": cur_shape.stroke_linejoin,
-    "stroke-linecap": cur_shape.stroke_linecap,
-    "stroke-opacity": cur_shape.stroke_opacity,
-    "opacity": cur_shape.opacity,
+    "fill": $elem.attr('fill'),
+    "fill-opacity": $elem.attr('fill-opacity'),
+    "stroke": $elem.attr('stroke'),
+    "stroke-width": $elem.attr('stroke-width'),
+    "stroke-dasharray": $elem.attr('stroke-dasharray'),
+    "stroke-linejoin": $elem.attr('stroke-linejoin'),
+    "stroke-linecap": $elem.attr('stroke-linecap'),
+    "stroke-opacity": $elem.attr('stroke-opacity'),
+    "opacity": $elem.attr('opacity'),
     "visibility":"hidden"
   };
 
