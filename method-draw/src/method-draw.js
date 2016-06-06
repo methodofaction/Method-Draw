97c6fe3038052e5538ad3911f8b4518f2d224c80
Toosick <preston_tighe@yahoo.com>
2016-06-06 00:53:54 -0500

    Update method-draw.js

--- method-draw/src/method-draw.js
+++ method-draw/src/method-draw.js
@@ -2979,7 +2979,7 @@
       
       // Test for embedImage support (use timeout to not interfere with page load)
       setTimeout(function() {
-        svgCanvas.embedImage('images/placeholder.svg', function(datauri) {
+        svgCanvas.embedImage(curConfig.imgPath + 'placeholder.svg', function(datauri) {
           if(!datauri) {
             // Disable option
             $('#image_save_opts [value=embed]').attr('disabled','disabled');
