850ce68c6368c189053b9522e7cfd47d65088af5
Mark MacKay <mark@duopixel.com>
2021-06-21 13:36:38 -0500

    improving first load

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,7 +1,8 @@
 (function(){
   const canvasContent = localStorage.getItem("md-canvasContent");
   const isDark = localStorage.getItem("md-darkmode");
-  if (!isDark) document.body.classList.add("inverted");
+  console.log(isDark)
+  if (!isDark && isDark !== null) document.body.classList.add("inverted");
   if (!canvasContent) return;
   const parser = new DOMParser();
   const doc = parser.parseFromString(canvasContent, "image/svg+xml");
